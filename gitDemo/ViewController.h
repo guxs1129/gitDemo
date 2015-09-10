165e7bdba8e3bc46cc0099d90ca418deb28383ee
guxs1129 <guxs1129@163.com>
2015-09-10 10:54:05 +0800

    Initial Commit

---
+++ gitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitDemo
+//
+//  Created by 顾新生 on 15/9/10.
+//  Copyright (c) 2015年 guxinsheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitDemo
+//
+//  Created by 顾新生 on 15/9/10.
+//  Copyright (c) 2015年 guxinsheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
